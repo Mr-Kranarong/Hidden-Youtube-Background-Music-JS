7968f203bd37c50f1926a8b6ca519c16b83e57ff
12th Mothership 404 - Cavalry Division <55760976+Mr-Kranarong@users.noreply.github.com>
2019-12-10 03:22:33 +0700

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,19 +1,3 @@
-/* 
- Hidden Youtube Background Music JS
- --------------------
- Original Author: Amit Agarwal
- Editor: Kranarong Kuanhat
- 
- Usage:
- <div data-video="VIDEO-ID"  
-        data-autoplay="1"         
-        data-loop="1"             
-        id="youtube-audio">
-        <script src="https://www.youtube.com/iframe_api"></script>
-        <script src="https://raw.githubusercontent.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/master/yt.js"></script>
- </div>
-*/
-
 function onYouTubeIframeAPIReady() {
     var e = document.getElementById("youtube-audio");
     var a = document.createElement("div");
