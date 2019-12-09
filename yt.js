3d043dadc51f5e4a49613c95ae1f390872cf4b6e
12th Mothership 404 - Cavalry Division <55760976+Mr-Kranarong@users.noreply.github.com>
2019-12-10 04:13:14 +0700

    Update yt.js

--- yt.js
+++ yt.js
@@ -11,7 +11,7 @@
         data-playlist="PLAYLIST-LEAVE EMPTY IF ONLY SINGLE SONG"             
         id="youtube-audio">
         <script src="https://www.youtube.com/iframe_api"></script>
-        <script src="https://rawcdn.githack.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/7968f203bd37c50f1926a8b6ca519c16b83e57ff/yt.js"></script>
+        <script src="https://rawcdn.githack.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/0d0d34dbb33614e1c99f85ad9899e2397f17770b/yt.js"></script>
  </div>
 */
 
