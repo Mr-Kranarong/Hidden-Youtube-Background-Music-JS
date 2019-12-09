f43ccd579a1c55bddefb08125bb586004613a635
12th Mothership 404 - Cavalry Division <55760976+Mr-Kranarong@users.noreply.github.com>
2019-12-10 03:11:10 +0700

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,9 +1,31 @@
 /* 
- YouTube Audio Embed 
+ Hidden Youtube Background Music JS
  --------------------
+ Original Author: Amit Agarwal
+ Editor: Kranarong Kuanhat
  
- Author: Amit Agarwal
- Web: http://www.labnol.org/?p=26740 
+ Usage:
+ <div data-video="VIDEO-ID"  
+        data-autoplay="1"         
+        data-loop="1"             
+        id="youtube-audio">
+        <script src="https://www.youtube.com/iframe_api"></script>
+        <script src="https://raw.githubusercontent.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/master/yt.js"></script>
+ </div>
 */
 
-function onYouTubeIframeAPIReady(){var e=document.getElementById("youtube-audio"),t=document.createElement("img");t.setAttribute("id","youtube-icon"),t.style.cssText="cursor:pointer;cursor:hand",e.appendChild(t);var a=document.createElement("div");a.setAttribute("id","youtube-player"),e.appendChild(a);var o=function(e){var a=e?"IDzX9gL.png":"quyUPXN.png";t.setAttribute("src","https://i.imgur.com/"+a)};e.onclick=function(){r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))};var r=new YT.Player("youtube-player",{height:"0",width:"0",videoId:e.dataset.video,playerVars:{autoplay:e.dataset.autoplay,loop:e.dataset.loop},events:{onReady:function(e){r.setPlaybackQuality("small"),o(r.getPlayerState()!==YT.PlayerState.CUED)},onStateChange:function(e){e.data===YT.PlayerState.ENDED&&o(!1)}}})}
+function onYouTubeIframeAPIReady() {
+    var e = document.getElementById("youtube-audio");
+    var a = document.createElement("div");
+    a.setAttribute("id", "youtube-player"), e.appendChild(a);
+    var r = new YT.Player("youtube-player", {
+        height: "0",
+        width: "0",
+        videoId: e.dataset.video,
+        playerVars: {
+            autoplay: e.dataset.autoplay,
+            loop: e.dataset.loop,
+            playlist: e.dataset.video
+        }
+    })
+}
